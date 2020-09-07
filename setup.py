29d6517587e091243be6ea411bf5cb9b882dda33
Neil Lyons <nwjlyons@googlemail.com>
2020-09-07 12:41:00 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='django_template_component',
+    packages=[],
+    version='0.1.0',
+    description='Inclusion tag with slots',
+    author='Neil Lyons',
+    license='BSD',
+    author_email='stdin@neillyons.io',
+    url='https://github.com/nwjlyons/django_template_component',
+    keywords=['inclustion tag', 'block', 'slot', 'template', 'django', ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
